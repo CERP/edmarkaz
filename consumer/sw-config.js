b2d62583537701c25895428f67d55711eff5afa2
Taimur Shah <taimur38@gmail.com>
2019-12-17 20:23:03 +0500

    cache fonts

--- consumer/sw-config.js
+++ consumer/sw-config.js
@@ -1,6 +1,6 @@
 workbox.routing.registerRoute(
 	({ url, event }) => {
-		const match = url.host === "storage.googleapis.com"
+		const match = url.host === "storage.googleapis.com" || url.host === "fonts.googleapis.com"
 		return match
 	},
 	new workbox.strategies.StaleWhileRevalidate({
