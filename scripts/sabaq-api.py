e74a327e3e5726bad44c472205d7268dc1f2cd71
Taimur Shah <taimur38@gmail.com>
2020-06-15 22:02:33 +0500

    more tweaks

--- scripts/sabaq-api.py
+++ scripts/sabaq-api.py
@@ -26,17 +26,31 @@ for v in lines:
 
     v_id = int(q['id'][0])
     v_obj = [x for x in api_resp if x['Id'] == v_id]
+
     if len(v_obj) == 0:
         not_found_in_api.append({**v, 'id': v_id})
         continue
 
-    mapped_link = prefix + v_obj[0]['UrduPath'][1:]
+    rel = v_obj[0]
+    mapped_link = prefix + rel['UrduPath'][1:]
     if v['Medium'] == 'English':
-        mapped_link = prefix + v_obj[0]['EnglishPath'][1:]
+        mapped_link = prefix + rel['EnglishPath'][1:]
 
-    mapped.append({**v, 'Link': mapped_link, 'sabaq_id': v_id})
+    mapped.append({
+        **v,
+        'Link': mapped_link,
+        'sabaq_id': v_id,
+        'topic_id': rel['TopicId'],
+        'subtopic_id': rel['SubTopicId']
+    })
 
 with open('mapped_urdu_portal.csv', 'w') as f:
-    writer = csv.DictWriter(f, [*fieldnames, 'sabaq_id'])
+    writer = csv.DictWriter(
+        f, [*fieldnames, 'sabaq_id', 'topic_id', 'subtopic_id'])
     writer.writeheader()
     writer.writerows(mapped)
+
+
+# Get Assessments data, and generate json
+
+# you could directly ingest into postgres
