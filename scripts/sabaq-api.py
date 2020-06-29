9a7d4d391947d23eebb2ac41b587e5956308eb01
ali ahmad <raoaliahmad03@gmail.com>
2020-06-30 01:46:41 +0500

    fixed duplicate assessments bug

--- scripts/sabaq-api.py
+++ scripts/sabaq-api.py
@@ -4,7 +4,7 @@ import requests
 import psycopg2 as pg
 from urllib.parse import urlparse, parse_qs
 
-with open('sabaq_june28_formatted.csv') as f:
+with open('/home/ali/dev/cerp/edmarkaz/scripts/sabaq_june28_formatted.csv') as f:
     reader = csv.DictReader(f)
     fieldnames = reader.fieldnames
     lines = [l for l in reader]
@@ -144,79 +144,79 @@ for val in mapped:
     subtopic_id = val["subtopic_id"]
 
     if topic_id in ex_dict:
-        for st in ex_dict[topic_id]:
-            if st in ex_dict[topic_id]:
-                # EXERCISES HAVE ALL THE EXERCISES FOR THIS TOPIC
-                excercises = ex_dict[topic_id][st]
-                ex_with_qs_ans = {}
-                for k in excercises:
-                    if k in qs_dict:
-                        curr_ex = excercises[k]
-                        qs_for_ex = {}
-                        for q_id in qs_dict[k]:
-                            curr_qs = qs_dict[k][q_id]
-                            # Getting all the options for curr_qs
-                            ans_for_qs = {}
-                            if q_id in ans_dict:
-                                for ans_id in ans_dict[q_id]:
-                                    ans_for_qs[ans_id] = {
-                                        "answer": ans_dict[q_id][ans_id]["Answer"],
-                                        "correct_answer": ans_dict[q_id][ans_id]["CorrectAnswer"],
-                                        "urdu_answer": ans_dict[q_id][ans_id]["UrduAnswer"],
-                                        "image": addPrefix(ans_dict[q_id][ans_id]["ImagePath"]),
-                                        "audio": addPrefix(ans_dict[q_id][ans_id]["AudioPath"]),
-                                        "id": ans_dict[q_id][ans_id]["Id"],
-                                        "active": ans_dict[q_id][ans_id]["Active"]
-                                    }
-                            else:
-                                print(f"QS not found in ans dict id:{q_id}")
-                            qs_for_ex[q_id] = {
-                                "order": curr_qs["Order"],
-                                "id": curr_qs["Id"],
-                                "title": curr_qs["Question"],
-                                "title_urdu": curr_qs["UrduQuestion"],
-                                "response_limit": curr_qs["ResponseLimit"],
-                                "multi_response": curr_qs["IsMultipleResponseAllowed"],
-                                "active": curr_qs["Active"],
-                                "image": addPrefix(curr_qs["ImagePath"]),
-                                "urdu_image": addPrefix(curr_qs["UrduImagePath"]),
-                                "audio": addPrefix(curr_qs["AudioPath"]),
-                                "answers": ans_for_qs
-                            }
-                            ex_with_qs_ans[k] = {
-                                "type": curr_ex["ExerciseTypeId"],
-                                "id": curr_ex["Id"],
-                                "subject": val["Subject"],
-                                "grade": val["Grade"],
-                                "chapter_id": val["Unit No"],
-                                "lesson_id": val["Video No"],
-                                "title": curr_ex["ExerciseTitle"],
-                                "title_urdu": curr_ex["UrduTitle"],
-                                "description": curr_ex["Description"],
-                                "order": curr_ex["Order"],
-                                "time": curr_ex["Timer"],
-                                "total_marks": curr_ex["TotalMarks"],
-                                "active": curr_ex["Active"],
-                                "questions": qs_for_ex,
-                                "source": "Sabaq Muse"
-                            }
-                    else:
-                        print(f"No question Found for {k}")
+        # Here is the problem shouldn't loop over all of thhe subtopics. Just check if it's in the dict
+        if subtopic_id in ex_dict[topic_id]:
+            # EXERCISES HAVE ALL THE EXERCISES FOR THIS TOPIC
+            excercises = ex_dict[topic_id][subtopic_id]
+            ex_with_qs_ans = {}
+            for k in excercises:
+                if k in qs_dict:
+                    curr_ex = excercises[k]
+                    qs_for_ex = {}
+                    for q_id in qs_dict[k]:
+                        curr_qs = qs_dict[k][q_id]
+                        # Getting all the options for curr_qs
+                        ans_for_qs = {}
+                        if q_id in ans_dict:
+                            for ans_id in ans_dict[q_id]:
+                                ans_for_qs[ans_id] = {
+                                    "answer": ans_dict[q_id][ans_id]["Answer"],
+                                    "correct_answer": ans_dict[q_id][ans_id]["CorrectAnswer"],
+                                    "urdu_answer": ans_dict[q_id][ans_id]["UrduAnswer"],
+                                    "image": addPrefix(ans_dict[q_id][ans_id]["ImagePath"]),
+                                    "audio": addPrefix(ans_dict[q_id][ans_id]["AudioPath"]),
+                                    "id": ans_dict[q_id][ans_id]["Id"],
+                                    "active": ans_dict[q_id][ans_id]["Active"]
+                                }
+                        else:
+                            print(f"QS not found in ans dict id:{q_id}")
+                        qs_for_ex[q_id] = {
+                            "order": curr_qs["Order"],
+                            "id": curr_qs["Id"],
+                            "title": curr_qs["Question"],
+                            "title_urdu": curr_qs["UrduQuestion"],
+                            "response_limit": curr_qs["ResponseLimit"],
+                            "multi_response": curr_qs["IsMultipleResponseAllowed"],
+                            "active": curr_qs["Active"],
+                            "image": addPrefix(curr_qs["ImagePath"]),
+                            "urdu_image": addPrefix(curr_qs["UrduImagePath"]),
+                            "audio": addPrefix(curr_qs["AudioPath"]),
+                            "answers": ans_for_qs
+                        }
+                        ex_with_qs_ans[k] = {
+                            "type": curr_ex["ExerciseTypeId"],
+                            "id": curr_ex["Id"],
+                            "subject": val["Subject"],
+                            "grade": val["Grade"],
+                            "chapter_id": val["Unit No"],
+                            "lesson_id": val["Video No"],
+                            "title": curr_ex["ExerciseTitle"],
+                            "title_urdu": curr_ex["UrduTitle"],
+                            "description": curr_ex["Description"],
+                            "order": curr_ex["Order"],
+                            "time": curr_ex["Timer"],
+                            "total_marks": curr_ex["TotalMarks"],
+                            "active": curr_ex["Active"],
+                            "questions": qs_for_ex,
+                            "source": "Sabaq Muse"
+                        }
+                else:
+                    print(f"No question Found for {k}")
 
-                print("==========================================\n")
-                # HERE I CAN INGEST IN DB EACH TOPICS ASSESSMENTS ---HOPEFULLY :D
-                for l in ex_with_qs_ans:
-                    ques = ex_with_qs_ans[l].pop("questions")
-                    meta = ex_with_qs_ans[l]
-                    id = f'{val["Medium"]}-{val["Grade"]}-{val["Subject"]}-{val["Unit No"]}-{val["Video No"]}-{ex_with_qs_ans[l]["order"]}'
-                    cur.execute(
-                        """INSERT INTO assessments (id, medium, class, subject, chapter_id, lesson_id, meta, questions)
+            print("==========================================\n")
+            # HERE I CAN INGEST IN DB EACH TOPICS ASSESSMENTS ---HOPEFULLY :D
+            for l in ex_with_qs_ans:
+                ques = ex_with_qs_ans[l].pop("questions")
+                meta = ex_with_qs_ans[l]
+                id = f'{val["Medium"]}-{val["Grade"]}-{val["Subject"]}-{val["Unit No"]}-{val["Video No"]}-{ex_with_qs_ans[l]["order"]}'
+                cur.execute(
+                    """INSERT INTO assessments (id, medium, class, subject, chapter_id, lesson_id, meta, questions)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO UPDATE SET meta=excluded.meta, questions=excluded.questions;""", (id, val["Medium"], val["Grade"], val["Subject"], val["Unit No"], val["Video No"], json.dumps(meta), json.dumps(ques)))
-                    conn.commit()
-                    print("DONE")
+                conn.commit()
+                print("DONE")
 
-            else:
-                print("Didnt found sub-topic in exercises")
+        else:
+            print(f"Didnt found sub-topic in exercises", subtopic_id)
     else:
         print("Didnt found topic in exercises")
 
