c9458ebc5e318b168789a1773a8ca6707c4676db
HumnaAleem <humna.aleem97@gmail.com>
2020-07-09 20:48:14 +0500

    resolve routing issue by getting grade from params

--- consumer/src/pages/Library/options.tsx
+++ consumer/src/pages/Library/options.tsx
@@ -1,192 +1,251 @@
-import React, { useEffect, useState } from 'react'
-import { getLessons } from '../../actions';
-import { connect } from 'react-redux';
-import { Link, RouteComponentProps } from 'react-router-dom';
-import LoadingIcon from '../../icons/load.svg'
-import { getIconsFromSubject } from '../../utils/getIconsFromSubject';
-import { Container, Avatar, Divider, Typography } from '@material-ui/core';
-import KAcad from '../../icons/KAcad.png'
-import KAcad_kids from '../../icons/KAcad_kids.png'
-import TAbad from '../../icons/TAbad.png'
-import { getDeviceOS } from '../../utils/getDeviceOS';
+import React, { useEffect, useState } from "react";
+import { getLessons } from "../../actions";
+import { connect } from "react-redux";
+import { Link, RouteComponentProps } from "react-router-dom";
+import LoadingIcon from "../../icons/load.svg";
+import { getIconsFromSubject } from "../../utils/getIconsFromSubject";
+import { Container, Avatar, Divider, Typography } from "@material-ui/core";
+import KAcad from "../../icons/KAcad.png";
+import KAcad_kids from "../../icons/KAcad_kids.png";
+import TAbad from "../../icons/TAbad.png";
+import { getDeviceOS } from "../../utils/getDeviceOS";
 
 interface P {
-	lessons: RootReducerState["lessons"]["db"]
-	lesson_loading: RootReducerState["lessons"]["loading"]
-	getLessons: () => void
+  lessons: RootReducerState["lessons"]["db"];
+  lesson_loading: RootReducerState["lessons"]["loading"];
+  getLessons: () => void;
 }
 
 interface Routeinfo {
-	medium: string
+  medium: string;
+  grade: string;
 }
 
-type Props = P & RouteComponentProps<Routeinfo>
-const getFeaturedApps = () => ([
-	{
-		title: "Sabaq Muse",
-		link_play: "http://bit.ly/muselessons",
-		link_app: "http://bit.ly/muselessons",
-		logo: "https://storage.googleapis.com/ilmx-product-images/Sabaq%20logo.png"
-	},
-	{
-		title: "Knowledge Platform",
-		link_play: "https://play.google.com/store/apps/details?id=com.knowledgeplatform.lsp&hl=en",
-		link_app: "https://play.google.com/store/apps/details?id=com.knowledgeplatform.lsp&hl=en",
-		logo: "https://storage.googleapis.com/ilmx-product-images/kp%20logo%20new.png"
-	},
-	{
-		title: "Taleemabad Primary",
-		link_play: "http://bit.ly/taleemabad",
-		link_app: "http://bit.ly/taleemabad",
-		logo: TAbad
-	},
-	{
-		title: "Taleemabad Secondary",
-		link_play: "http://bit.ly/taleemabad",
-		link_app: "http://bit.ly/taleemabad",
-		logo: TAbad
-	},
-	{
-		title: "Khan Academy",
-		link_play: "https://play.google.com/store/apps/details?id=org.khanacademy.android",
-		link_app: "https://apps.apple.com/us/app/khan-academy/id469863705",
-		logo: KAcad
-	},
-	{
-		title: "Khan Academy Kids",
-		link_play: "https://play.google.com/store/apps/details?id=org.khankids.android&hl=en",
-		link_app: "https://apps.apple.com/us/app/khan-academy-kids/id1378467217",
-		logo: KAcad_kids
-	},
-])
+type Props = P & RouteComponentProps<Routeinfo>;
+const getFeaturedApps = () => [
+  {
+    title: "Sabaq Muse",
+    link_play: "http://bit.ly/muselessons",
+    link_app: "http://bit.ly/muselessons",
+    logo: "https://storage.googleapis.com/ilmx-product-images/Sabaq%20logo.png",
+  },
+  {
+    title: "Knowledge Platform",
+    link_play:
+      "https://play.google.com/store/apps/details?id=com.knowledgeplatform.lsp&hl=en",
+    link_app:
+      "https://play.google.com/store/apps/details?id=com.knowledgeplatform.lsp&hl=en",
+    logo:
+      "https://storage.googleapis.com/ilmx-product-images/kp%20logo%20new.png",
+  },
+  {
+    title: "Taleemabad Primary",
+    link_play: "http://bit.ly/taleemabad",
+    link_app: "http://bit.ly/taleemabad",
+    logo: TAbad,
+  },
+  {
+    title: "Taleemabad Secondary",
+    link_play: "http://bit.ly/taleemabad",
+    link_app: "http://bit.ly/taleemabad",
+    logo: TAbad,
+  },
+  {
+    title: "Khan Academy",
+    link_play:
+      "https://play.google.com/store/apps/details?id=org.khanacademy.android",
+    link_app: "https://apps.apple.com/us/app/khan-academy/id469863705",
+    logo: KAcad,
+  },
+  {
+    title: "Khan Academy Kids",
+    link_play:
+      "https://play.google.com/store/apps/details?id=org.khankids.android&hl=en",
+    link_app: "https://apps.apple.com/us/app/khan-academy-kids/id1378467217",
+    logo: KAcad_kids,
+  },
+];
 
-const StudentPortalOptions: React.FC<Props> = ({ getLessons, lessons, lesson_loading, match }) => {
+const StudentPortalOptions: React.FC<Props> = ({
+  getLessons,
+  lessons,
+  lesson_loading,
+  match,
+}) => {
+  useEffect(() => {
+    getLessons();
+  }, [getLessons]);
 
-	useEffect(() => {
-		getLessons()
-	}, [getLessons])
+  const subjectSortArray = [
+    "English",
+    "Math",
+    "Urdu",
+    "Science and Technology",
+    "The World Around Us",
+    "Hamari Dunya",
+    "Art & Music",
+    "Ethics & Islam",
+    "Islamiat",
+    "Me & My Health",
+    "Physics",
+    "Biology",
+    "Chemistry",
+  ];
+  const medium = match.params.medium;
+  const classGrade = match.params.grade ? match.params.grade : "";
+  const [grade, setGrade] = useState("");
 
-	const subjectSortArray = [
-		"English",
-		"Math",
-		"Urdu",
-		"Science and Technology",
-		"The World Around Us",
-		"Hamari Dunya",
-		"Art & Music",
-		"Ethics & Islam",
-		"Islamiat",
-		"Me & My Health",
-		"Physics",
-		"Biology",
-		"Chemistry"
-	]
+  return lesson_loading ? (
+    <div className="loading">
+      <img className="icon" src={LoadingIcon} alt="loading-icon" />
+      <div className="text">Fetching Lessons</div>
+    </div>
+  ) : (
+    <div className="student-portal-op">
+      {lessons[medium] ? (
+        <Container maxWidth="md">
+          <div className="row">
+            {classGrade === "" && <div className="title">Select Class</div>}
+            {classGrade === "" ? (
+              <div className="content">
+                {lessons[medium]["Preschool"] && (
+                  <Link to={`/library/${medium}/${`Preschool`}`}>
+                    <div className="oval">Preschool</div>
+                  </Link>
+                )}
+                {lessons[medium]["KG"] && (
+                  <Link to={`/library/${medium}/${`KG`}`}>
+                    <div className="oval">KG</div>
+                  </Link>
+                )}
+                {Object.keys(lessons[medium])
+                  .filter((g) => g !== "Preschool" && g !== "KG")
+                  .sort((a, b) => parseFloat(a) - parseFloat(b))
+                  .map((g) => {
+                    return (
+                      <Link to={`/library/${medium}/${g}`}>
+                        <div
+                          className={
+                            classGrade === g ? "bg-blue oval " : "oval"
+                          }
+                          key={g}
+                        >{`Class ${g}`}</div>
+                      </Link>
+                    );
+                  })}
+              </div>
+            ) : (
+              <div className="content selected">
+                <div className="bg-blue oval">
+                  {classGrade !== "Preschool" && classGrade !== "KG"
+                    ? `Class ${classGrade}`
+                    : classGrade}
+                </div>
+              </div>
+            )}
+          </div>
+          {classGrade && classGrade && (
+            <div className="row">
+              <div className="title">Select Subject</div>
+              <div className="content">
+                {Object.keys(lessons[medium][classGrade])
+                  .sort((a, b) =>
+                    subjectSortArray.indexOf(a) < subjectSortArray.indexOf(b)
+                      ? -1
+                      : 1
+                  )
+                  .map((s) => {
+                    return (
+                      <Link
+                        to={`/library/${medium}/${classGrade}/${s}`}
+                        className="square subject"
+                        style={{
+                          backgroundImage: `url(${getIconsFromSubject(s)})`,
+                        }}
+                        key={s}
+                      >
+                        {s}
+                      </Link>
+                    );
+                  })}
+              </div>
+            </div>
+          )}
+          <Divider />
+        </Container>
+      ) : (
+        <Container maxWidth="sm">
+          <Typography
+            variant="h5"
+            color="textSecondary"
+            align="center"
+            gutterBottom
+          >
+            We couldn't find anything. Please write to us via{" "}
+            <a href="tel:0348-1119-119">Sms</a>
+            <br />
+            or
+            <a href="https://api.whatsapp.com/send?phone=923481119119">
+              Whatsapp
+            </a>
+            , and help us make Ilmexchange better for you.
+          </Typography>
+          <Divider />
+        </Container>
+      )}
+      <Typography
+        style={{ margin: "20px 5px 0px" }}
+        color="textSecondary"
+        align="center"
+        variant="h5"
+      >
+        Featured Apps
+      </Typography>
+      <Container
+        maxWidth="sm"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {getFeaturedApps().map((p) => {
+          return (
+            <a
+              href={getDeviceOS() === "Android" ? p.link_play : p.link_app}
+              target="__blank"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                textDecoration: "none",
+                margin: "5px",
+                padding: "5px",
+                color: "#474747",
+              }}
+            >
+              <Avatar
+                style={{ margin: "5px", objectFit: "contain" }}
+                variant="rounded"
+              >
+                <img className="partner-avatar" alt="" src={p.logo} />
+              </Avatar>
+              <Typography variant="subtitle2">{p.title}</Typography>
+            </a>
+          );
+        })}
+      </Container>
+    </div>
+  );
+};
 
-	const medium = match.params.medium
-	const [grade, setGrade] = useState("");
-
-	return lesson_loading ? <div className="loading">
-		<img className="icon" src={LoadingIcon} alt="loading-icon" />
-		<div className="text">Fetching Lessons</div>
-	</div> : <div className="student-portal-op">
-
-
-			{lessons[medium] ? <Container maxWidth="md">
-				<div className="row">
-					{grade === "" && <div className="title">Select Class</div>}
-					{
-						grade === "" ? <div className="content">
-							{lessons[medium]["Preschool"] && <div className="oval" onClick={(e) => setGrade("Preschool")}>Preschool</div>}
-							{lessons[medium]["KG"] && <div className="oval" onClick={(e) => setGrade("KG")}>KG</div>}
-							{
-								Object.keys(lessons[medium])
-									.filter(g => g !== "Preschool" && g !== "KG")
-									.sort((a, b) => parseFloat(a) - parseFloat(b))
-									.map(g => {
-										return <div className={grade === g ? "bg-blue oval " : "oval"} key={g} onClick={(e) => setGrade(g)}>{`Class ${g}`}</div>
-									})
-							}
-						</div> : <div className="content selected">
-								<div className="bg-blue oval">{grade !== "Preschool" && grade !== "KG" ? `Class ${grade}` : grade}</div>
-								<div style={{ textDecoration: "underline", color: "blue", cursor: "pointer" }} onClick={(e) => setGrade("")}>Change Class</div>
-							</div>
-					}
-				</div>
-				{grade && <div className="row">
-					<div className="title">Select Subject</div>
-					<div className="content">
-						{
-							Object.keys(lessons[medium][grade])
-								.sort((a, b) => subjectSortArray.indexOf(a) < subjectSortArray.indexOf(b) ? -1 : 1)
-								.map(s => {
-									return <Link
-										to={`/library/${medium}/${grade}/${s}`}
-										className="square subject"
-										style={{
-											backgroundImage: `url(${getIconsFromSubject(s)})`
-										}}
-										key={s}>{s}</Link>
-								})
-						}
-					</div>
-
-				</div>}
-				<Divider />
-			</Container> : <Container maxWidth="sm">
-					<Typography
-						variant="h5"
-						color="textSecondary"
-						align="center"
-						gutterBottom
-					>
-						We couldn't find anything.
-						Please write to us via <a href="tel:0348-1119-119">Sms</a>
-						<br />or <a href="https://api.whatsapp.com/send?phone=923481119119">Whatsapp</a>,
-						and help us make Ilmexchange better for you.
-					</Typography>
-					<Divider />
-				</Container>
-			}
-			<Typography
-				style={{ margin: "20px 5px 0px", }}
-				color="textSecondary"
-				align="center"
-				variant="h5">Featured Apps</Typography>
-			<Container maxWidth="sm" style={{
-				display: "flex",
-				justifyContent: "center",
-				flexWrap: "wrap"
-			}}>
-				{
-					getFeaturedApps()
-						.map(p => {
-							return <a
-								href={getDeviceOS() === "Android" ? p.link_play : p.link_app}
-								target="__blank"
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									alignItems: "center",
-									justifyContent: "center",
-									textDecoration: "none",
-									margin: "5px",
-									padding: "5px",
-									color: "#474747",
-								}}>
-								<Avatar style={{ margin: "5px", objectFit: "contain" }} variant="rounded">
-									<img className="partner-avatar" alt="" src={p.logo} />
-								</Avatar>
-								<Typography variant="subtitle2">{p.title}</Typography>
-							</a>
-						})
-				}
-			</Container>
-		</div>
-}
-
-export default connect((state: RootReducerState) => ({
-	lessons: state.lessons.db,
-	lesson_loading: state.lessons.loading
-}), (dispatch: Function) => ({
-	getLessons: () => dispatch(getLessons())
-}))(StudentPortalOptions)
+export default connect(
+  (state: RootReducerState) => ({
+    lessons: state.lessons.db,
+    lesson_loading: state.lessons.loading,
+  }),
+  (dispatch: Function) => ({
+    getLessons: () => dispatch(getLessons()),
+  })
+)(StudentPortalOptions);
--- consumer/src/pages/TabsBar/index.tsx
+++ consumer/src/pages/TabsBar/index.tsx
@@ -9,160 +9,205 @@ import ErrorComponent from "../../components/Error";
 import { submitError } from "../../actions/core";
 import { connect } from "react-redux";
 import Help from "../Help";
-import contactUs from '../../icons/contactUs.svg'
+import contactUs from "../../icons/contactUs.svg";
 import TrackedRoute from "../../components/TrackedRoute";
 import Library from "../Library";
 import LessonPage from "../Library/Lesson";
-import LibraryInstructionMedium from '../Library/medium'
-import StudentPortalOptions from '../Library/options';
+import LibraryInstructionMedium from "../Library/medium";
+import StudentPortalOptions from "../Library/options";
 import Layout from "../../components/Layout";
 import { Paper, Tabs, Tab } from "@material-ui/core";
 import StudentProfile from "../StudentPortal/studentProfile";
-import HelpFooter from "components/Footer/HelpFooter"
+import HelpFooter from "components/Footer/HelpFooter";
 
 import "./style.css";
 import SchoolDashboard from "pages/School/Dashboard";
 
 interface S {
-	tab: number
-	error: boolean;
-	err?: Error;
-	errInfo?: React.ErrorInfo;
+  tab: number;
+  error: boolean;
+  err?: Error;
+  errInfo?: React.ErrorInfo;
 }
 
 interface P {
-	token: RootReducerState["auth"]["token"]
-	connected: boolean;
-	user: RootReducerState["auth"]["user"]
-	sendError: (err: Error, errInfo: React.ErrorInfo) => void;
+  token: RootReducerState["auth"]["token"];
+  connected: boolean;
+  user: RootReducerState["auth"]["user"];
+  sendError: (err: Error, errInfo: React.ErrorInfo) => void;
 }
 
-interface RouteInfo { }
+interface RouteInfo {}
 
 type propTypes = RouteComponentProps<RouteInfo> & P;
 
 class TabsBar extends Component<propTypes, S> {
-	constructor(props: propTypes) {
-		super(props);
+  constructor(props: propTypes) {
+    super(props);
 
-		this.state = {
-			tab: this.getCurrentTab(this.props.location.pathname),
-			error: false,
-			err: undefined,
-			errInfo: undefined
-		};
-	}
+    this.state = {
+      tab: this.getCurrentTab(this.props.location.pathname),
+      error: false,
+      err: undefined,
+      errInfo: undefined,
+    };
+  }
 
-	getCurrentTab = (path: string) => {
+  getCurrentTab = (path: string) => {
+    if (path.split("/").some((i) => i === "dashboard")) {
+      return 0;
+    }
+    if (path.split("/").some((i) => i === "bazaar" || i === "supplier")) {
+      return 2;
+    }
+    if (path.split("/").some((i) => i === "library")) {
+      return 1;
+    }
+    if (path.split("/").some((i) => i === "help")) {
+      return 3;
+    }
+    return 4;
+  };
 
-		if (path.split("/").some(i => i === "dashboard")) {
-			return 0
-		}
-		if (path.split("/").some(i => i === "bazaar" || i === "supplier")) {
-			return 2
-		}
-		if (path.split("/").some(i => i === "library")) {
-			return 1
-		}
-		if (path.split("/").some(i => i === "help")) {
-			return 3
-		}
-		return 4
-	}
+  componentDidUpdate(prevProps: propTypes) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.setState({
+        tab: this.getCurrentTab(this.props.location.pathname),
+      });
+    }
+  }
 
-	componentDidUpdate(prevProps: propTypes) {
-		if (this.props.location.pathname !== prevProps.location.pathname) {
-			this.setState({
-				tab: this.getCurrentTab(this.props.location.pathname)
-			})
-		}
-	}
+  componentDidCatch(err: Error, errInfo: React.ErrorInfo) {
+    this.props.sendError(err, errInfo);
 
+    this.setState({
+      error: true,
+      err,
+      errInfo,
+    });
+  }
 
-	componentDidCatch(err: Error, errInfo: React.ErrorInfo) {
-		this.props.sendError(err, errInfo);
+  handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    this.setState({ tab: newValue });
+  };
 
-		this.setState({
-			error: true,
-			err,
-			errInfo
-		});
-	}
+  render() {
+    const { location, user, history } = this.props;
+    const current = location.pathname;
 
-	handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    const library = location.pathname.split("/").some((i) => i === "library");
 
-		this.setState({ tab: newValue })
-	};
+    if (this.state.error && this.state.err && this.state.errInfo) {
+      return (
+        <ErrorComponent error={this.state.err} errInfo={this.state.errInfo} />
+      );
+    }
 
-	render() {
+    const callLink = this.props.connected
+      ? "https://api.whatsapp.com/send?phone=923481119119"
+      : "tel:0348-1119-119";
 
-		const { location, user, history } = this.props
-		const current = location.pathname;
-
-		const library = location.pathname.split("/").some(i => i === "library")
-
-		if (this.state.error && this.state.err && this.state.errInfo) {
-			return (
-				<ErrorComponent error={this.state.err} errInfo={this.state.errInfo} />
-			);
-		}
-
-		const callLink = this.props.connected ?
-
-			"https://api.whatsapp.com/send?phone=923481119119" : "tel:0348-1119-119"
-
-		return user === undefined ? <Redirect to="" /> : <Layout>
-			<div className="tabs-page">
-				{(user === "SCHOOL" && current !== "/profile" && current !== "/start-mob" && current !== "/log-in") &&
-					<Paper style={{ flexGrow: 1 }}>
-						<Tabs
-							onChange={this.handleTabChange}
-							value={this.state.tab}
-							indicatorColor="primary"
-							textColor="primary"
-							centered
-						>
-							<Tab label="Dashboard" onClick={() => history.push("/dashboard")} />
-							<Tab label="Library" onClick={() => history.push("/library")} />
-							<Tab label="Bazaar" onClick={() => history.push("/bazaar")} />
-							<Tab label="Help" onClick={() => history.push("/help")} />
-						</Tabs>
-					</Paper>}
-				<>
-					<TrackedRoute exact path="/supplier/:supplier_id/:product_id" component={ProductPage} />
-					<TrackedRoute exact path="/supplier/:supplier_id" component={SupplierHome} />
-					<TrackedRoute path="/profile" component={Profile} />
-					<Route path="/articles/:article_id" component={ArticleRouter} />
-					<TrackedRoute exact path="/articles" component={Articles} />
-					<TrackedRoute path="/help" component={Help} />
-					<TrackedRoute exact path="/library" component={LibraryInstructionMedium} />
-					<TrackedRoute exact path="/library/:medium" component={StudentPortalOptions} />
-					<TrackedRoute exact path="/library/:medium/:grade/:subject" component={Library} />
-					<TrackedRoute exact path="/library/:medium/:grade/:subject/:chapter/:chapter_name" component={LessonPage} />
-					<TrackedRoute exact path="/bazaar" component={ProductHome} />
-					<TrackedRoute exact path="/student-profile" component={StudentProfile} />
-					<TrackedRoute exact path="/dashboard" component={SchoolDashboard} />
-				</>
-				{/* {!library && <a className="contact-us" href={callLink}>
+    return user === undefined ? (
+      <Redirect to="" />
+    ) : (
+      <Layout>
+        <div className="tabs-page">
+          {user === "SCHOOL" &&
+            current !== "/profile" &&
+            current !== "/start-mob" &&
+            current !== "/log-in" && (
+              <Paper style={{ flexGrow: 1 }}>
+                <Tabs
+                  onChange={this.handleTabChange}
+                  value={this.state.tab}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  centered
+                >
+                  <Tab
+                    label="Dashboard"
+                    onClick={() => history.push("/dashboard")}
+                  />
+                  <Tab
+                    label="Library"
+                    onClick={() => history.push("/library")}
+                  />
+                  <Tab label="Bazaar" onClick={() => history.push("/bazaar")} />
+                  <Tab label="Help" onClick={() => history.push("/help")} />
+                </Tabs>
+              </Paper>
+            )}
+          <>
+            <TrackedRoute
+              exact
+              path="/supplier/:supplier_id/:product_id"
+              component={ProductPage}
+            />
+            <TrackedRoute
+              exact
+              path="/supplier/:supplier_id"
+              component={SupplierHome}
+            />
+            <TrackedRoute path="/profile" component={Profile} />
+            <Route path="/articles/:article_id" component={ArticleRouter} />
+            <TrackedRoute exact path="/articles" component={Articles} />
+            <TrackedRoute path="/help" component={Help} />
+            <TrackedRoute
+              exact
+              path="/library"
+              component={LibraryInstructionMedium}
+            />
+            <TrackedRoute
+              exact
+              path="/library/:medium/:grade"
+              component={StudentPortalOptions}
+            />
+            <TrackedRoute
+              exact
+              path="/library/:medium"
+              component={StudentPortalOptions}
+            />
+            <TrackedRoute
+              exact
+              path="/library/:medium/:grade/:subject"
+              component={Library}
+            />
+            <TrackedRoute
+              exact
+              path="/library/:medium/:grade/:subject/:chapter/:chapter_name"
+              component={LessonPage}
+            />
+            <TrackedRoute exact path="/bazaar" component={ProductHome} />
+            <TrackedRoute
+              exact
+              path="/student-profile"
+              component={StudentProfile}
+            />
+            <TrackedRoute exact path="/dashboard" component={SchoolDashboard} />
+          </>
+          {/* {!library && <a className="contact-us" href={callLink}>
 					<img src={contactUs} alt="phone" />
 					<div>Contact Us</div>
 				</a>} */}
 
-				{(current !== "/" && current !== "/bazaar" && current !== "/about-us") && <>
-					<HelpFooter hlink={callLink} />
-				</>}
-			</div>
-		</Layout>
-	}
+          {current !== "/" && current !== "/bazaar" && current !== "/about-us" && (
+            <>
+              <HelpFooter hlink={callLink} />
+            </>
+          )}
+        </div>
+      </Layout>
+    );
+  }
 }
 export default connect(
-	(state: RootReducerState) => ({
-		connected: state.connected,
-		token: state.auth.token,
-		user: state.auth.user
-	}),
-	(dispatch: Function) => ({
-		sendError: (err: Error, errInfo: React.ErrorInfo) =>
-			dispatch(submitError(err, errInfo))
-	})
+  (state: RootReducerState) => ({
+    connected: state.connected,
+    token: state.auth.token,
+    user: state.auth.user,
+  }),
+  (dispatch: Function) => ({
+    sendError: (err: Error, errInfo: React.ErrorInfo) =>
+      dispatch(submitError(err, errInfo)),
+  })
 )(TabsBar);
