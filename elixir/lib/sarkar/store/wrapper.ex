7535a3b74d788f89164f2c465f79955736543254
ali ahmad <raoaliahmad03@gmail.com>
2020-07-21 19:23:40 +0500

    added timeout to pg wrapper

--- elixir/lib/sarkar/store/wrapper.ex
+++ elixir/lib/sarkar/store/wrapper.ex
@@ -1,5 +1,5 @@
 defmodule EdMarkaz.DB.Postgres do
 	def query(db, querystring, params, opts \\ []) do
-		Postgrex.query(db, querystring, params, pool: DBConnection.Poolboy)
+		Postgrex.query(db, querystring, params, pool: DBConnection.Poolboy, timeout: 60000)
 	end
 end
