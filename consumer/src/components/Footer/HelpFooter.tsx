b306f0ad8f4e8307d48366fafb18842d3ca37c8d
Humna Aleem <60597485+HumnaAleem@users.noreply.github.com>
2020-07-29 22:04:04 +0500

    expand footer on click expand more icon in mobile mode (#51)
    * expand footer on click expand more icon in mobile mode
    
    * set zIndex of footer
    
    * text change
    
    Co-authored-by: Humna Aleem <humna.aleem@cerp.org.pk>
    Co-authored-by: ali ahmad <raoaliahmad03@gmail.com>

--- consumer/src/components/Footer/HelpFooter.tsx
+++ consumer/src/components/Footer/HelpFooter.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import { Box, Grid, Button } from '@material-ui/core'
-import { YouTube, Phone, Facebook } from '@material-ui/icons'
+import { YouTube, Phone, Facebook, Height } from '@material-ui/icons'
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
 import Modal from '../../components/Modal'
 import { getIDFromYoutbeLink } from 'utils/getIdFromYoutubeLink'
 import Youtube from 'react-youtube'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 type PropsType = {
 	hlink?: string
@@ -22,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
 		paddingBottom: theme.spacing(1)
 	},
 	footer: {
+		zIndex: 1,
 		left: 0,
 		right: 0,
 		bottom: 0,
@@ -43,6 +46,16 @@ const useStyles = makeStyles((theme) => ({
 	},
 	fbIcon: {
 		fill: '#385898'
+	},
+	footerHeadingDiv: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	expandLessIcon: {
+		float: 'right',
+		color: '#1BB4BB',
+		padding: '10px'
 	}
 }));
 
@@ -59,6 +72,16 @@ const HelpFooter: React.FC<PropsType> = ({ hlink }) => {
 	const classes = useStyles();
 	const fbLink = "https://web.facebook.com/groups/ilmexchangediscussionforum/"
 	const [showModal, setShowModal] = useState(false);
+	const [toggleFooter, setToggleFooter] = useState(false);
+	const [width, setWidth] = useState(window.innerWidth)
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize)
+		return () => {
+			window.removeEventListener('resize', handleResize)
+		}
+	})
+
 	const pathname = window.location.pathname;
 
 	const getYoutubeTutorialLink = (pathname: any) => {
@@ -78,75 +101,158 @@ const HelpFooter: React.FC<PropsType> = ({ hlink }) => {
 		setShowModal(false)
 	}
 
-	return (
-		<footer className={classes.footer}>
-			{showModal ? <Modal >
-				<div className="modal-box video-modal">
-					{
-						isYoutubeUrl(getYoutubeTutorialLink(pathname)) ? <Youtube
-							videoId={getIDFromYoutbeLink(getYoutubeTutorialLink(pathname))}
-							className={'iframe'}
-							opts={{
-								width: "100%",
-								height: "100%",
-								playerVars: {
-									rel: 0,
-									showinfo: 0,
-									autoplay: 1
-								}
+	const handleResize = () => {
+		setWidth(window.innerWidth)
+	}
 
-							}} />
-							:
-							<video className="iframe" src={getYoutubeTutorialLink(pathname)} controls autoPlay>
-								your browser doesnt support html video, please update it and use chrome for best experience.
+	const expandMore = () => {
+		setToggleFooter(!toggleFooter)
+	}
+
+	return (
+		<>
+			{width <= 425 ? <footer className={classes.footer}>
+				{showModal ? <Modal >
+					<div className="modal-box video-modal">
+						{
+							isYoutubeUrl(getYoutubeTutorialLink(pathname)) ? <Youtube
+								videoId={getIDFromYoutbeLink(getYoutubeTutorialLink(pathname))}
+								className={'iframe'}
+								opts={{
+									width: "100%",
+									height: "100%",
+									playerVars: {
+										rel: 0,
+										showinfo: 0,
+										autoplay: 1
+									}
+
+								}} />
+								:
+								<video className="iframe" src={getYoutubeTutorialLink(pathname)} controls autoPlay>
+									your browser doesnt support html video, please update it and use chrome for best experience.
 					</video>
-					}
-					<div className="button" style={{ marginTop: '5px', backgroundColor: "#f05967" }} onClick={() => onBack()}>Back</div>
-				</div>
-			</Modal> : null}
-			<Container maxWidth="md" className={classes.main}>
-				<div style={{ marginLeft: "1.50rem", marginRight: "1.50rem" }}>
-					<Grid container spacing={0} alignItems="center" justify="center">
-						<Grid item xs={12} sm={3}>
-							<Typography
-								className={classes.heading6}
-								variant="h6">
-								Need help?
-							</Typography>
-						</Grid>
-						<Grid container item xs={12} sm={9} spacing={1} justify="center">
-							<Grid item xs={6} sm={3}>
-								<Button
-									size="medium"
-									variant="outlined"
-									className={classes.helpButton}
-									startIcon={<YouTube style={{ fill: "red" }} />}
-									onClick={relevantTutorial}
-								> Tutorial </Button>
-							</Grid>
-							<Grid item xs={6} sm={3}>
-								<Button
-									size="medium"
-									variant="outlined"
-									className={classes.helpButton}
-									startIcon={<Phone className={classes.phoneIcon} />}
-									href={hlink}
-								> Contact Us </Button>
-							</Grid>
+						}
+						<div className="button" style={{ marginTop: '5px', backgroundColor: "#f05967" }} onClick={() => onBack()}>Back</div>
+					</div>
+				</Modal> : null}
+				<Container maxWidth="md" className={classes.main}>
+					<div style={{ marginLeft: "1.50rem", marginRight: "1.50rem" }}>
+						<Grid container spacing={0} alignItems="center" justify="center">
 							<Grid item xs={12} sm={3}>
-								<Button
-									size="medium"
-									variant="outlined"
-									className={classes.helpButton}
-									startIcon={<Facebook className={classes.fbIcon} />}
-									href={fbLink}
-								> Discussion </Button>
+								<div className={classes.footerHeadingDiv}>
+									<Typography
+										className={classes.heading6}
+										variant="h6">
+										Need help?
+									</Typography>
+									{
+										toggleFooter ? <ExpandMoreIcon className={classes.expandLessIcon} onClick={expandMore} /> : <ExpandLessIcon className={classes.expandLessIcon} onClick={expandMore} />
+									}
+								</div>
 							</Grid>
+							{toggleFooter ? <Grid container item xs={12} sm={9} spacing={1} justify="center">
+								<Grid item xs={12} sm={3}>
+									<Button
+										size="medium"
+										variant="outlined"
+										className={classes.helpButton}
+										startIcon={<YouTube style={{ fill: "red" }} />}
+										onClick={relevantTutorial}
+									> Tutorial </Button>
+								</Grid>
+								<Grid item xs={12} sm={3}>
+									<Button
+										size="medium"
+										variant="outlined"
+										className={classes.helpButton}
+										startIcon={<Phone className={classes.phoneIcon} />}
+										href={hlink}
+									> Contact Us </Button>
+								</Grid>
+								<Grid item xs={12} sm={3}>
+									<Button
+										size="medium"
+										variant="outlined"
+										className={classes.helpButton}
+										startIcon={<Facebook className={classes.fbIcon} />}
+										href={fbLink}
+									> Discussion </Button>
+								</Grid>
+							</Grid>
+								: null}
 						</Grid>
-					</Grid>
-				</div>
-			</Container>
-		</footer >
+					</div>
+				</Container>
+			</footer > : <footer className={classes.footer}>
+					{showModal ? <Modal >
+						<div className="modal-box video-modal">
+							{
+								isYoutubeUrl(getYoutubeTutorialLink(pathname)) ? <Youtube
+									videoId={getIDFromYoutbeLink(getYoutubeTutorialLink(pathname))}
+									className={'iframe'}
+									opts={{
+										width: "100%",
+										height: "100%",
+										playerVars: {
+											rel: 0,
+											showinfo: 0,
+											autoplay: 1
+										}
+
+									}} />
+									:
+									<video className="iframe" src={getYoutubeTutorialLink(pathname)} controls autoPlay>
+										your browser doesnt support html video, please update it and use chrome for best experience.
+					</video>
+							}
+							<div className="button" style={{ marginTop: '5px', backgroundColor: "#f05967" }} onClick={() => onBack()}>Back</div>
+						</div>
+					</Modal> : null}
+					<Container maxWidth="md" className={classes.main}>
+						<div style={{ marginLeft: "1.50rem", marginRight: "1.50rem" }}>
+							<Grid container spacing={0} alignItems="center" justify="center">
+								<Grid item xs={12} sm={3}>
+									<Typography
+										className={classes.heading6}
+										variant="h6">
+										Need help?
+							</Typography>
+								</Grid>
+								<Grid container item xs={12} sm={9} spacing={1} justify="center">
+									<Grid item xs={12} sm={3}>
+										<Button
+											size="medium"
+											variant="outlined"
+											className={classes.helpButton}
+											startIcon={<YouTube style={{ fill: "red" }} />}
+											onClick={relevantTutorial}
+										> Tutorial </Button>
+									</Grid>
+									<Grid item xs={12} sm={3}>
+										<Button
+											size="medium"
+											variant="outlined"
+											className={classes.helpButton}
+											startIcon={<Phone className={classes.phoneIcon} />}
+											href={hlink}
+										> Contact Us </Button>
+									</Grid>
+									<Grid item xs={12} sm={3}>
+										<Button
+											size="medium"
+											variant="outlined"
+											className={classes.helpButton}
+											startIcon={<Facebook className={classes.fbIcon} />}
+											href={fbLink}
+										> Discussion </Button>
+									</Grid>
+								</Grid>
+							</Grid>
+						</div>
+					</Container>
+				</footer >}
+		</>
 	)
 }
 
