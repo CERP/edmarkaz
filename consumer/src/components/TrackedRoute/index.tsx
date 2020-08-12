5f9fdd1f8ca2846296c1989234ac68c44d7079be
ali ahmad <raoaliahmad03@gmail.com>
2020-08-12 16:22:47 +0500

    init commit-online analytics done

--- consumer/src/components/TrackedRoute/index.tsx
+++ consumer/src/components/TrackedRoute/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, RouteProps } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { trackRoute } from '../../actions'
+
+
+declare global {
+	interface Window {
+		gtag?: (
+			key: string,
+			trackingId: string,
+			config: { page_path: string }
+		) => void
+	}
+}
 
 type propsType = {
 	trackRoute: (path: string) => any;
@@ -12,6 +23,9 @@ const TrackedRoute = ({ component, trackRoute, ...rest }: propsType) => {
 	const Component = component
 
 	return <Route {...rest} render={(props) => {
+		if (window.gtag) {
+			window.gtag('config', 'UA-174568735-1', { page_path: window.location.pathname })
+		}
 		trackRoute(window.location.pathname)
 		//@ts-ignore
 		return <Component {...props} />
