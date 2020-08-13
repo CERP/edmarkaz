92f4566eb6f9b47cf95831dc08b1f07fb080f412
Humna Aleem <humna.aleem@cerp.org.pk>
2020-08-13 17:28:16 +0500

    made changes in challenge page

--- consumer/src/pages/Challenge/index.tsx
+++ consumer/src/pages/Challenge/index.tsx
@@ -1,22 +1,21 @@
-import * as React from "react"
+import * as React from 'react'
 import poster from './poster.jpg'
-
+import Header from '../../components/Header'
 import './style.css'
 
 function Challenge(props: any) {
 	return <div>
-		<div className="poster-div" style={{
-		}}>
+		<Header path={"/"} />
+		<div className="poster-div" >
 			<h3 className="independence-heading">Happy Independence Day</h3>
 			<div className="content-div">
-				<div className="quotes-div">
-					<div className="quotes">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a ullamcorper risus, at euismod sem. Fusce tincidunt quam purus, in sagittis nisi tristique ut. Duis eget elementum neque. Vivamus posuere bibendum quam in interdum. Cras ac quam quam. Morbi eu enim vestibulum, ultrices metus et, porttitor enim. Sed volutpat mauris vitae ex lobortis, id lobortis felis dictum. Praesent blandit nisi cursus nunc venenatis, sit amet porta mauris semper. Integer pretium dui in rutrum finibus. Quisque vel mi et sem tincidunt mattis. Mauris dapibus, enim sit amet tincidunt volutpat, orci dolor laoreet est, et sagittis ipsum lectus eu nibh. Morbi efficitur est eget elit congue, eu dictum justo semper. Quisque vitae elementum risus. Ut quis diam venenatis, eleifend purus in, pulvinar mauris. Sed vitae arcu nibh. Nulla in maximus metus.</div>
-				</div>
+				<div className="quotes">
+					"Pakistan not only means freedom and independence but the Muslim Ideology which has to be preserved, which has come to us as a precious gift and treasure and which, we hope other will share with us"
+					</div>
 				<img className="poster" src={poster} alt="img" />
 			</div>
-		</div>
-	</div >
+		</div >
+	</div>
 }
 
 export default Challenge
