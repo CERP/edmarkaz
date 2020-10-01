da64be6da53a7cf2bcb5b05cb993fee91d53b43e
Humna Aleem <humna.aleem@cerp.org.pk>
2020-10-01 12:17:04 +0500

    made requested changes

--- elixir/lib/sarkar/telenor/telenor.ex
+++ elixir/lib/sarkar/telenor/telenor.ex
@@ -12,7 +12,7 @@ defmodule EdMarkaz.Telenor do
 			{:ok, res} ->
 				result = res.body |> xpath(~x"data/text()"l)
 				[session_id] = result
-				EdMarkaz.EtsStore.insert({"session_id", to_string(session_id), :os.system_time(:millisecond)})
+				:ets.insert(:telenor_ets, {"session_id", to_string(session_id), :os.system_time(:millisecond)})
 				{:ok, to_string(session_id)}
 
 			{:error, err} ->
@@ -26,52 +26,47 @@ defmodule EdMarkaz.Telenor do
 
 	def send_sms(number, text) do
 
-		session_obj = EdMarkaz.EtsStore.get("session_id")
+		session_obj = :ets.lookup(:telenor_ets, "session_id")
 
 		case Enum.empty?(session_obj) do
 			true ->
 				case get_session_id() do
 				{:ok, res} ->
-					case get("sendsms.jsp", query: [session_id: res, to: number, text: text, mask: System.get_env("TELENOR_MASK")]) do
-						{:ok, res} ->
-							{:ok, res.body}
-						{:error, err} ->
-							IO.puts "SEND SMS ERROR to #{number}"
-							{:error, err}
-					end
+					send(res, number, text)
 				{:error, msg} ->
 					{:error, msg}
 				end
 			false ->
-				[{_, session_id, timestamp}] = session_obj
+				[head | tail] = session_obj
+				{_, session_id, timestamp} = head
 				curr_time = :os.system_time(:millisecond)
 				half_hr = 30*60*1000
 
 				case timestamp + half_hr > curr_time do
 					true ->
-						case get("sendsms.jsp", query: [session_id: session_id, to: number, text: text, mask: System.get_env("TELENOR_MASK")]) do
-							{:ok, res} ->
-								{:ok, res.body}
-							{:error, err} ->
-								IO.puts "SEND SMS ERROR to #{number}"
-								{:error, err}
-						end
+						send(session_id, number, text)
 					false ->
 						case get_session_id() do
 							{:ok, res} ->
-								case get("sendsms.jsp", query: [session_id: res, to: number, text: text, mask: System.get_env("TELENOR_MASK")]) do
-									{:ok, res} ->
-										{:ok, res.body}
-									{:error, err} ->
-										IO.puts "SEND SMS ERROR to #{number}"
-										{:error, err}
-								end
+								send(res, number, text)
 							{:error, msg} ->
 								{:error, msg}
 						end
 				end
 		end
 
+	end
+
+	def send(session_id, number, text) do
+
+		case get("sendsms.jsp", query: [session_id: session_id, to: number, text: text, mask: System.get_env("TELENOR_MASK")]) do
+			{:ok, res} ->
+				{:ok, res.body}
+			{:error, err} ->
+				IO.puts "SEND SMS ERROR to #{number}"
+				IO.inspect err
+				{:error, err}
+		end
 	end
 
 end
