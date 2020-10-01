92321d1653b90f4b0c11de7f42a730d8f7087b55
Humna Aleem <humna.aleem@cerp.org.pk>
2020-10-01 15:24:42 +0500

    rename function and remove mask from bash file

--- elixir/lib/sarkar/telenor/telenor.ex
+++ elixir/lib/sarkar/telenor/telenor.ex
@@ -32,23 +32,22 @@ defmodule EdMarkaz.Telenor do
 			true ->
 				case get_session_id() do
 				{:ok, res} ->
-					send(res, number, text)
+					send_sms(res, number, text)
 				{:error, msg} ->
 					{:error, msg}
 				end
 			false ->
-				[head | tail] = session_obj
-				{_, session_id, timestamp} = head
+				[{_, session_id, timestamp} | _] = session_obj
 				curr_time = :os.system_time(:millisecond)
 				half_hr = 30*60*1000
 
 				case timestamp + half_hr > curr_time do
 					true ->
-						send(session_id, number, text)
+						send_sms(session_id, number, text)
 					false ->
 						case get_session_id() do
 							{:ok, res} ->
-								send(res, number, text)
+								send_sms(res, number, text)
 							{:error, msg} ->
 								{:error, msg}
 						end
@@ -57,9 +56,9 @@ defmodule EdMarkaz.Telenor do
 
 	end
 
-	def send(session_id, number, text) do
+	defp send_sms(session_id, number, text) do
 
-		case get("sendsms.jsp", query: [session_id: session_id, to: number, text: text, mask: System.get_env("TELENOR_MASK")]) do
+		case get("sendsms.jsp", query: [session_id: session_id, to: number, text: text, mask: "ILMEXCHANGE"]) do
 			{:ok, res} ->
 				{:ok, res.body}
 			{:error, err} ->
