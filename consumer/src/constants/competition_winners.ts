7f27d8819368013390fd93e4c129058c8a580852
mudassar045 <mudassaralics007@gmail.com>
2020-10-02 10:42:04 +0500

    add competition winner data

---
+++ consumer/src/constants/competition_winners.ts
@@ -0,0 +1,133 @@
+export interface CompetitionWinner {
+	category: string
+	school: string
+	district: string
+	std_name: string
+	class: string
+	drawing_url: string
+}
+
+const CompetitionWinners: CompetitionWinner[] = [
+	{
+		category: "Good citizen",
+		school: "The Wisdom School",
+		district: "Sialkot",
+		std_name: "M. Shehzad",
+		class: "5",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/shahzad.jpeg"
+	},
+	{
+		category: "Good citizen",
+		school: "Punjab Kids & High School",
+		district: "Pakpatan",
+		std_name: "Maryam Fatima",
+		class: "3",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/maryam%20fatima.jpeg"
+	},
+	{
+		category: "Good citizen",
+		school: "Punjab Kids & High School",
+		district: "Pakpatan",
+		std_name: "M. Abdullah Munawwar",
+		class: "3",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/abdullah.jpeg"
+	},
+	{
+		category: "Good citizen",
+		school: "Progressive International Academy",
+		district: "HariPur",
+		std_name: "Habibullah Khan",
+		class: "4",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/Habibullah.jpeg"
+	},
+	{
+		category: "Good citizen",
+		school: "The Wisdom School",
+		district: "Sialkot",
+		std_name: "Ayeza azhar",
+		class: "5",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/aiza.jpeg"
+	},
+	{
+		category: "Good citizen",
+		school: "NFBE School",
+		district: "Shikarpur",
+		std_name: "Abdul Rehman, Paris, M Waqar, Sawaira",
+		class: "5",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/paris.jpeg"
+	},
+	{
+		category: "Favorite thing",
+		school: "Al Noor Educational Institute",
+		district: "Okara",
+		std_name: "Misbah",
+		class: "1",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/musya.jpeg"
+	},
+	{
+		category: "Favorite thing",
+		school: "NFBE School",
+		district: "Shikarpur",
+		std_name: "Faiqa Mustafa",
+		class: "1",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/faiqa%20and%20hamna.jpeg"
+	},
+	{
+		category: "Favorite thing",
+		school: "Punjab Kids & High School",
+		district: "Pakpatan",
+		std_name: "Ali Abdullah",
+		class: "2",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/ali.jpg"
+	},
+	{
+		category: "Favorite thing",
+		school: "NFBE School",
+		district: "Shikarpur",
+		std_name: "Muhammad Ali",
+		class: "2",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/m%20ali%2C%20salqain%20mahmmod.jpeg"
+	},
+	{
+		category: "Favorite thing",
+		school: "The Logic School System",
+		district: "Nankana Sahib",
+		std_name: "Aman Zahra",
+		class: "1",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/aman%20zahida.jpeg"
+	},
+	{
+		category: "Essay",
+		school: "Punjab Kids & High School",
+		district: "Pakpatan",
+		std_name: "Mahnoor",
+		class: "7",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/mahnoor.jpeg"
+	},
+	{
+		category: "Essay",
+		school: "Honest Science Academy",
+		district: "Ellahabad",
+		std_name: "Khadija Ashraf",
+		class: "8",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/khadija.jpeg"
+	},
+	{
+		category: "Essay",
+		school: "Punjab Kids & High School",
+		district: "Pakpatan",
+		std_name: "Hamna Liaquat",
+		class: "7",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/hamna%20liaqat.jpeg"
+	},
+	{
+		category: "Essay",
+		school: "Honest Science Academy",
+		district: "Ellahabad",
+		std_name: "Amna ",
+		class: "8",
+		drawing_url: "https://storage.googleapis.com/ilmx-product-images/amna.jpeg"
+	}
+]
+
+export { CompetitionWinners }
